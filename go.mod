d34e2852d5d8e76d149f542feb5cca1f17feac01
Shammah Chancellor <shammah.chancellor@gmail.com>
2019-07-01 21:57:49 -0700

    Add initial implementation of simple keydb (#3)
    Summary:
    This commit implements the basic Set/Get interface for the KeyDB which can
    validate that the data being set is allowed, prior to setting the key. This
    will allow building the REST API, and other abstractions that will be needed.
    
    Test Plan:
      `go test ./...`

--- go.mod
+++ go.mod
@@ -4,5 +4,20 @@ go 1.12
 
 require (
 	github.com/boltdb/bolt v1.3.1
+	github.com/coreos/go-systemd v0.0.0-20190620071333-e64a0ec8b42a // indirect
+	github.com/dchest/siphash v1.2.1 // indirect
+	github.com/etcd-io/bbolt v1.3.3
+	github.com/gcash/bchd v0.14.6
+	github.com/gcash/bchutil v0.0.0-20190625002603-800e62fe9aff
+	github.com/golang/protobuf v1.3.1
+	github.com/pkg/errors v0.8.1
 	github.com/rs/zerolog v1.14.3
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.3.0
+	go.etcd.io/bbolt v1.3.3
+	golang.org/x/crypto v0.0.0-20190621222207-cc06ce4a13d4 // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20190628222527-fb37f6ba8261 // indirect
 )
---
+++ pkg/keydb/keydb_test.go
@@ -0,0 +1,89 @@
+package keydb
+
+import (
+	"crypto/sha256"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/cashweb/keyserver/pkg/models"
+	"github.com/gcash/bchd/bchec"
+	"github.com/gcash/bchd/chaincfg"
+	"github.com/gcash/bchutil"
+	"github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddressSignVerify(t *testing.T) {
+	assert := assert.New(t)
+	privKey, err := bchec.NewPrivateKey(bchec.S256())
+	assert.Nil(err)
+	pubkey := privKey.PubKey()
+	t.Log(len(pubkey.SerializeCompressed()))
+	addr, err := bchutil.NewAddressPubKeyHash(bchutil.Hash160(pubkey.SerializeUncompressed()), &chaincfg.MainNetParams)
+	assert.Nil(err)
+	assert.Equal(addr.ScriptAddress(), bchutil.Hash160(pubkey.SerializeUncompressed()))
+	t.Log(addr.ScriptAddress())
+	sig, err := privKey.SignSchnorr(addr.ScriptAddress())
+	assert.Nil(err)
+	valid := sig.Verify(addr.ScriptAddress(), pubkey)
+	assert.True(valid)
+}
+
+func TestSetGet(t *testing.T) {
+	assert := assert.New(t)
+
+	// Setup a DB
+	dir, err := ioutil.TempDir("", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir) // clean up
+	cfg := &Config{
+		DBPath: filepath.Join(dir, "testsetget.db"),
+	}
+	keyDb, err := New(cfg)
+	assert.Nil(err)
+
+	// Generate a privkey
+	privKey, err := bchec.NewPrivateKey(bchec.S256())
+	assert.Nil(err)
+	pubkey := privKey.PubKey()
+	// Find the address (Note: legacy atm)
+	addr, err := bchutil.NewAddressPubKeyHash(bchutil.Hash160(pubkey.SerializeUncompressed()), &chaincfg.MainNetParams)
+	assert.Nil(err)
+
+	addrMetadata := &models.AddressMetadata{
+		PubKey: pubkey.SerializeUncompressed(),
+		Payload: &models.Payload{
+			Timestamp: time.Now().UnixNano(),
+			Rows: []*models.MetadataField{
+				&models.MetadataField{
+					Headers: []*models.Header{
+						&models.Header{
+							Name:  "Type",
+							Value: "EgoBoost",
+						},
+					},
+					Metadata: []byte("Shammah has such great ideas... or something"),
+				},
+			},
+		},
+	}
+	rawMetadata, err := proto.Marshal(addrMetadata.GetPayload())
+	assert.Nil(err)
+
+	msgHash := sha256.Sum256(rawMetadata)
+	sig, err := privKey.SignSchnorr(msgHash[:])
+	assert.Nil(err)
+	addrMetadata.Signature = sig.Serialize()
+
+	err = keyDb.Set(addr.EncodeAddress(), addrMetadata)
+	assert.Nil(err)
+	fetchedMetadata, err := keyDb.Get(addr.EncodeAddress())
+	assert.Nil(err)
+	assert.True(proto.Equal(addrMetadata, fetchedMetadata), "Fetch value did not match expected value")
+}
